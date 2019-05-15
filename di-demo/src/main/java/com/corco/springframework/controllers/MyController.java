35f2666e3165cf65dd76f7b0c80cd55e79088cc6
john <john@192.168.0.67>
2019-05-15 16:26:53 +0100

    adding annotations to enable Spring to handle the dependency Injection

--- di-demo/src/main/java/com/corco/springframework/controllers/MyController.java
+++ di-demo/src/main/java/com/corco/springframework/controllers/MyController.java
@@ -2,8 +2,20 @@ package com.corco.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import com.corco.springframework.services.GreetingService;
+
 @Controller
 public class MyController {
+	
+	private GreetingService greetingService;
+	
+	
+	public MyController(GreetingService greetingService) {
+		super();
+		this.greetingService = greetingService;
+	}
+
+
 	public void hello() {
 		System.out.println("Hello!!!");
 	}
--- di-demo/src/main/java/com/corco/springframework/controllers/SetterInjectedController.java
+++ di-demo/src/main/java/com/corco/springframework/controllers/SetterInjectedController.java
@@ -1,20 +1,21 @@
 package com.corco.springframework.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import com.corco.springframework.services.GreetingService;
 
 @Controller
-public class SetterInjectedController {
-	
-	@Autowired
+public class SetterInjectedController { 
 	private GreetingService greetingService;
 	
 	public String sayHello() {
 		return greetingService.sayGreeting();
 	}
 	
+	@Autowired
+	@Qualifier("setterGreetingService")
 	public void setGreetingService(GreetingService greetingService) {
 		this.greetingService = greetingService;
 	}
